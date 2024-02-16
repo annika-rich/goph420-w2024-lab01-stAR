6631141cbcf605c7f1cd8a18ea147be04be1272b
Annika Richardson <annika.richardson@ucalgary.ca>
2024-02-16 14:59:20 -0700

    created outline of driver scripts

---
+++ examples/driver_probability.py
@@ -0,0 +1,7 @@
+# Driver Script that uses a known function to perform numerical integration
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ examples/driver_seismic.py
@@ -0,0 +1,10 @@
+# Driver Script that uses discrete data to perform numerical integration
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    
+    vel_data = np.loadtxt('data/s_wave_data.txt')
+
+if __name__ == '__main__':
+    main()
