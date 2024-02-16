3c466c2a3be886639d83e3725ea8179f04869a29
Annika Richardson <annika.richardson@ucalgary.ca>
2024-02-16 14:52:25 -0700

    dded gauss-legendre functions documentation

--- src/goph420_lab01/integration.py
+++ src/goph420_lab01/integration.py
@@ -27,3 +27,34 @@ def integrate_newton(x, f, alg = "trap"):
         If the string flag contains a str other than trap or simp
         If the dimensions of x and f are incompatible
     """
+
+    def inegrate_gauss(f, lims, npts = 3):
+        """Performs numerical integration of a function using Gauss-Legendre quadrature.
+
+        Parameters
+        ----------
+        f:  reference to a callable object
+            e.g., funciton of class that implements the __call__() method
+
+        lims:   object, len == 2
+                contains the lower and upper bounds of integration (x = a, x = b)
+
+        npts:   integer, optional
+                gives number of integration points to use
+                Possible inputs are 1, 2, 3, 4, 5
+
+        Returns
+        -------
+        float, float-like
+            Provides the integral estimate
+
+        Raises
+        ------
+        TypeError:
+            If f is not callable
+
+        ValueError:
+            If lims does not have len == 2
+            If lims[0] or lims[1] is not convertible to float
+            If npts is not in [1, 2, 3, 4, 5]
+        """
